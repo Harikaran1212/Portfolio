4795e7884cb91f4625dfb3981f5621d4b7a62692
Harikaran R <131746803+Harikaran1212@users.noreply.github.com>
2025-06-23 08:33:40 +0530

    Update page.tsx

--- app/dashboard/home/page.tsx
+++ app/dashboard/home/page.tsx
@@ -24,7 +24,7 @@ const HomePage   = () => {
             
             <div className='flex flex-col items-center justify-center gap-5'>
                 <Image
-                    src="/assets/harikaran.png"
+                    src="/assets/chatgpt.png"
                     alt="Harikaran" 
                     width={200}
                     height={200}
